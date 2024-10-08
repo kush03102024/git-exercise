3fe3baa39a3a4bd1dcb18e44d1126c192188eeb5
kush03102024 <kush.patel@simformsolutions.com>
2024-10-08 11:23:07 +0530

    Add comments for adding the logic for get and post method.

--- api.py
+++ api.py
@@ -2,6 +2,10 @@ class PythonGetPostAPI:
 
     def get(self):
         print("GET method.")
+        # Add logic for the GET method.
+        print("End of the GET method.")
 
     def post(self):
         print("POST method.")
+        # Add logic for the POST method.
+        print("End of the POST method.")
